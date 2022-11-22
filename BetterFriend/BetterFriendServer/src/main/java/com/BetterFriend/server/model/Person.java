232b7030c6fb687dfe2c7604fc23ad559107b30a
Gianni Parello <giparello90@gmail.com>
2022-11-22 11:07:12 -0500

    added user model to server

--- BetterFriend/BetterFriendServer/src/main/java/com/BetterFriend/server/model/Person.java
+++ BetterFriend/BetterFriendServer/src/main/java/com/BetterFriend/server/model/Person.java
@@ -1,5 +1,90 @@
 package com.BetterFriend.server.model;
 
-public class Person {
+import java.util.Date;
 
+public class Person {
+    int friendId;
+    int userId;
+    String lastName;
+    String firstName;
+    int phoneNumber;
+    String city;
+    Date lastTimeCalled;
+    Date birthDay;
+
+    public Person(int friendId, int userId, String lastName, String firstName, int phoneNumber, String city,Date lastTimeCalled, Date birthDay){
+        this.friendId = friendId;
+        this.userId = userId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.lastTimeCalled = lastTimeCalled;
+        this.birthDay = birthDay;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getFriendId() {
+        return friendId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public Date getLastTimeCalled() {
+        return lastTimeCalled;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setFriendId(int friendId) {
+        this.friendId = friendId;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public void setLastTimeCalled(Date lastTimeCalled) {
+        this.lastTimeCalled = lastTimeCalled;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
+
