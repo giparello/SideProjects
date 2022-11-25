3833c53e3ac9f1f3600771dc52195cb9032935e7
Gianni Parello <giparello90@gmail.com>
2022-11-25 00:12:58 -0500

    added notebook

---
+++ NoteBook/src/test/java/com/NoteBook/NoteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoteBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
