c8965c8f6c4684546243d08454e4e8cfb10b3e90
Gianni Parello <giparello90@gmail.com>
2022-12-04 21:18:05 -0500

    added some styling and layout

--- Brightspot/article-website-ui/src/components/ArticleList.vue
+++ Brightspot/article-website-ui/src/components/ArticleList.vue
@@ -5,13 +5,14 @@
       <article>
           
           <ul class="customHeight" v-for="article in articles" v-bind:key="article.articleId">
-              <li>
+              <li class="article">
               <p>{{article.articleId}}</p>
               <h1>{{article.articleName}}</h1>
-              <p>{{article.articleMainPoint}}</p>
+              <h4>{{article.articleMainPoint}}</h4>
               <p>{{article.authorName}}</p>
               <p class="row bottomGap">{{article.articleBody}}</p>
               <p class="dateHolder">{{article.dateWritten}}</p>
+              <!--DECIDE IF YOU WANT TO ADD IMAGES OR NOT-->
               </li>
           </ul>
       </article>
@@ -35,7 +36,18 @@ export default {
 </script>
 
 <style>
-
+article {
+    display: grid;
+    grid-template-columns: 1fr 740px 1fr;
+    list-style: none;
+}
+article > * {
+    grid-column: 2;
+}
+article > figure {
+    grid-column: 1 / -1;
+    margin: 20px 0;
+}
 /* Single column display for phones */
 @media (max-width: 459px) {
   .archive {
