0a9b2bc524112fc29a1db61bc9d08910559b97a4
Gianni Parello <giparello90@gmail.com>
2022-12-05 20:53:39 -0500

    created cards for blog layout

--- Brightspot/article-website-ui/src/components/ArticleList.vue
+++ Brightspot/article-website-ui/src/components/ArticleList.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
       <h1>Articles</h1>
-
       <article>
-          
-          <ul class="customHeight" v-for="article in articles" v-bind:key="article.articleId">
-              <li class="article">
-              <p>{{article.articleId}}</p>
-              <h1>{{article.articleName}}</h1>
-              <img> {{article.imageURL}}<img>
-              <h4>{{article.articleMainPoint}}</h4>
-              <p>{{article.authorName}}</p>
-              <p class="row bottomGap">{{article.articleBody}}</p>
-              <p class="dateHolder">{{article.dateWritten}}</p>
-              <!--DECIDE IF YOU WANT TO ADD IMAGES OR NOT-->
+          <ul class="entire-archive" v-for="article in articles" v-bind:key="article.articleId">
+              <li class="article-container">
+                  <div class="information">
+                    <p class="date-written">Date Written: {{article.dateWritten}}</p>
+                    <p class="author-name">Author: {{article.authorName}}</p>
+                    <p class="article-id">Article no. {{article.articleId}}</p>
+                  </div>
+                  <div class="body">
+                    <h1 class="article-header">{{article.articleName}}</h1>
+                    <h2 class="article-subheader">{{article.articleMainPoint}}</h2>
+                    <p class="article-body">{{article.articleBody}}</p>
+                  </div>
+                  <div class="image">
+                    <img v-bind:src="article.imageURL" class ="article-image"/>
+                  </div>
               </li>
           </ul>
       </article>
@@ -37,18 +40,47 @@ export default {
 </script>
 
 <style>
-article {
-    display: grid;
-    grid-template-columns: 1fr 740px 1fr;
+html{
+    font-family: sans-serif;
+    font-size: 10px;
+}
+.article-container{
+    width: 90%;
+    height: 1fr;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+    padding: 0 1.5rem;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.archive h1{
+    letter-spacing: 1px;
+    font-size: 50px;
+    color: #6968aa;
+    padding: 10px;
+    border: 2px dashed #0181a0;
+    text-transform: uppercase;
+    display: inline-block;
+    text-align: center;
+    margin-top: 90px;
+    margin-left: 375px;
+}
+.article-container{
+    margin-top: 150px;
+}
+.article-image{
+    width: 100%;
+    position: relative;
+    z-index: 1;
+}
+li{
     list-style: none;
 }
-article > * {
-    grid-column: 2;
-}
-article > figure {
-    grid-column: 1 / -1;
-    margin: 20px 0;
-}
+
+
+
 /* Single column display for phones */
 @media (max-width: 459px) {
   .archive {
